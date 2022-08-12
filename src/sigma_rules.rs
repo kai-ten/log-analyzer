22ddba6a7e3e16d5e5a150457a178419a49b1c07
Kai <0x4b6169@gmail.com>
2022-08-12 18:36:40 -0400

    Move yml functionality and tests, refactor

--- src/sigma_rules.rs
+++ src/sigma_rules.rs
@@ -1,17 +1,10 @@
 use std::collections::BTreeMap;
 use std::io::BufReader;
+use anyhow::Error;
 use serde::{Serialize, Deserialize};
 use serde_yaml::{Mapping, Value};
-use crate::yml::{self, deserialize_yml};
+use crate::yml::{self, deserialize_yml, is_yml};
 use walkdir::{DirEntry, WalkDir};
-
-#[derive(Debug)]
-enum RequiredFields {
-    title,
-    logSource,
-    detection,
-    condition,
-}
 
 #[derive(Default, Serialize, Deserialize, PartialEq, Debug)]
 struct Logsource {
@@ -51,89 +44,89 @@ impl SigmaRule {
     //     fn contains_string(){}
 
     // This adds all of the rules in the config/rules folder upon initialization
-    pub fn add_rules() {
+    pub fn add_rules() -> Result<(), Error> {
         for file in WalkDir::new("config/rules").into_iter().filter_map(|file| file.ok()) {
-            if file.metadata().unwrap().is_file() && SigmaRule::is_yml(&file) {
+            if file.metadata().unwrap().is_file() && is_yml(&file) {
 
-                println!("{}", file.path().display());
+                let de_yml = deserialize_yml(file.path().display().to_string())?;
+                let yml_value = Value::deserialize(de_yml)?;
+
+                println!("{:?}", yml_value.is_mapping());
+                if let true = yml_value.is_mapping() {
+                    let sigmaRule = SigmaRule::build_sigma_rule(&yml_value);
+                } else {
+                    continue;
+                }
+
 
             }
         }
+        Ok(())
     }
 
-    fn is_yml(entry: &DirEntry) -> bool {
-        entry.file_name()
-            .to_str()
-            .map(|s| s.ends_with(".yml"))
-            .unwrap_or(false)
+    fn build_sigma_rule(yml_value: &Value) -> Result<(), Error> {
+        // dbg!("okok = {:?}", yml_mapping.get("logsource")?.get("service")?.as_str());
+
+        dbg!("SDLK");
+        let yml_mapping = yml_value.as_mapping().unwrap();
+        println!("N = {:?}", yml_mapping);
+
+        // println!("OK = {:?}", yml_mapping.get("logsource")?);
+        // println!("bool = {:?}", yml_mapping.get("logsource")?.is_mapping());
+
+        // let b = if let Some(b) = ... { b } else { ... };
+        // let logsource = yml_mapping.get("logsource")?;
+        // let category = logsource.get("category").unwrap_or(&Value::Null);
+        // let service = logsource.get("service").unwrap_or(&Value::Null);
+        // println!("OMG = {:?}", category.as_str());
+        // if let false = category.is_null() {category.as_str()?.to_string()};
+
+        // println!("OKDS = {:?}", service.as_str());
+        // println!("NICE = {:?}", category.as_str());
+
+        // let logsource = Logsource {
+        //     category: Some(yml_mapping.get("logsource")?.get("category")?.as_str()?.to_string()),
+        //     service: Some("".to_string()),
+        //     product: Some(yml_mapping.get("logsource")?.get("product")?.as_str()?.to_string()),
+        // };
+
+        // let rule = SigmaRule {
+        //     title: {
+        //         yml_mapping.get("title")?.as_str()?.to_string()
+        //     },
+        //     id: Some(yml_mapping.get("id")?.as_str()?.to_string()),
+        //     status: Some(yml_mapping.get("status")?.as_str()?.to_string()),
+        //     description: Some("".to_string()),
+        //     references: Some(vec![]),
+        //     tags: Some(vec![]),
+        //     authors: Some("".to_string()),
+        //     date: Some("".to_string()),
+        //     logsource,
+        //     detections: Default::default(),
+        //     fields: Some(vec![]),
+        //     falsepositives: Some(vec![]),
+        //     level: Some("".to_string()),
+        // };
+
+        // println!("rule = {:?}", rule);
+        Ok(())
     }
 
-    // fn build_sigma_rule(yml_mapping: &Value) -> Option<SigmaRule> {
-    //     dbg!("{:?}", yml_mapping);
-    //
-    //     // dbg!("okok = {:?}", yml_mapping.get("logsource")?.get("service")?.as_str());
-    //
-    //     dbg!("SDLK");
-    //     let nice = yml_mapping.get("logsource")?.get("service");
-    //     // if let true = nice.is_none() {
-    //     //     println!("WOOO");
-    //     // } else {
-    //     //     println!("NOOO");
-    //     // }
-    //
-    //
-    //     // dbg!("THIS = {:?}", nice);
-    //
-    //
-    //     let logsource = Logsource {
-    //         category: Some(yml_mapping.get("logsource")?.get("category")?.as_str()?.to_string()),
-    //         service: Some("".to_string()),
-    //         product: Some(yml_mapping.get("logsource")?.get("product")?.as_str()?.to_string()),
-    //     };
-    //
-    //     let rule = SigmaRule {
-    //         title: {
-    //             yml_mapping.get("title")?.as_str()?.to_string()
-    //         },
-    //         id: Some(yml_mapping.get("id")?.as_str()?.to_string()),
-    //         status: Some(yml_mapping.get("status")?.as_str()?.to_string()),
-    //         description: Some("".to_string()),
-    //         references: Some(vec![]),
-    //         tags: Some(vec![]),
-    //         authors: Some("".to_string()),
-    //         date: Some("".to_string()),
-    //         logsource,
-    //         detections: Default::default(),
-    //         fields: Some(vec![]),
-    //         falsepositives: Some(vec![]),
-    //         level: Some("".to_string()),
-    //     };
-    //
-    //     println!("rule = {:?}", rule);
-    //     Some(rule)
-    // }
-    //
-    // fn is_none() -> bool {
-    //
-    //
-    //     return false;
-    // }
+    fn is_none(field: Option<&Value>) -> bool {
+
+        if field.is_none() {
+            false
+        } else {
+            true
+        }
+
+    }
 
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn return_yml_as_string() {
-//         let test = Nice();
-//
-//         // println!("{:?}", test);
-//         // println!("{}", test.title)
-//         // Ok(_) => assert!(false, "db file should not exist"),
-//         // Err(read_yaml()) => assert!(true),
-//
-//         // assert_eq!((), read_yaml().unwrap());
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+}
--- src/yml.rs
+++ src/yml.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::BufReader;
 use serde_yaml::Deserializer;
+use walkdir::DirEntry;
 
 pub fn deserialize_yml<'de>(path: String) -> std::io::Result<Deserializer<'de>> {
     let file = File::open(path)?;
@@ -9,8 +10,16 @@ pub fn deserialize_yml<'de>(path: String) -> std::io::Result<Deserializer<'de>> 
     Ok(de_yml)
 }
 
+pub fn is_yml(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.ends_with(".yml"))
+        .unwrap_or(false)
+}
+
 #[cfg(test)]
 mod test_yml_deserialization {
+    use walkdir::WalkDir;
     use super::*;
 
     #[test]
@@ -23,6 +32,22 @@ mod test_yml_deserialization {
     fn de_yml_file_that_does_not_exist() {
         let yaml = deserialize_yml("test/assets/should_not_exist.yml".to_string());
         assert_eq!(yaml.is_err(), true, "File does not exist")
+    }
+
+    #[test]
+    fn is_yml_file() {
+        for file in WalkDir::new("test/assets/simple.yml").into_iter().filter_map(|file| file.ok()) {
+            let is_yml = is_yml(&file);
+            assert_eq!(is_yml, true, "Is a yml file")
+        };
+    }
+
+    #[test]
+    fn is_not_yml_file() {
+        for file in WalkDir::new("test/assets/is_not_yml_file/").into_iter().filter_map(|file| file.ok()) {
+            let is_yml = is_yml(&file);
+            assert_eq!(is_yml, false, "Is not a yml file")
+        };
     }
 }
 
